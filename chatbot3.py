ab029ce0b8fa9f20d50ddc8218a5d90691e71954
antonio venancio <antoniovenancio10@gmail.com>
2024-11-07 22:36:22 -0300

    ajustes

--- chatbot3.py
+++ chatbot3.py
@@ -37,11 +37,11 @@ def model_hf_hub(model="meta-llama/Meta-Llama-3-8B-Instruct", temperature=0.1):
       model_kwargs={
           "temperature": temperature,
           "return_full_text": False,
-          "max_new_tokens": 512,
+          "max_new_tokens": 512
           #"stop": ["<|eot_id|>"],
           # demais parâmetros que desejar
-      }
-  )
+        }
+    ) 
   return llm
 
 def model_openai(model="gpt-4o-mini", temperature=0.1):
